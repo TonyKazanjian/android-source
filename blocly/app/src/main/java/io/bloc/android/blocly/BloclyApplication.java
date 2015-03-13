54112e96d4d350b4d4d370297efd3765f1c19084
Tony Kazanjian <tony.kazanjian@gmail.com>
2015-03-12 23:12:32 -0700

    Checkpoint 35 assignment completed

--- blocly/app/src/main/java/io/bloc/android/blocly/BloclyApplication.java
+++ blocly/app/src/main/java/io/bloc/android/blocly/BloclyApplication.java
@@ -2,17 +2,19 @@ package io.bloc.android.blocly;
 
 import android.app.Application;
 
+import io.bloc.android.blocly.api.DataSource;
+
 /**
  * Created by tonyk_000 on 3/11/2015.
  */
 
     public class BloclyApplication extends Application {
 
-        // #1
+        // #1 - creates singleton instance of BloclyApplication and DataSource
         public static BloclyApplication getSharedInstance() {
             return sharedInstance;
         }
-
+        // #1 and 2 allow any class from any part of the application to grab a reference to these objects
         // #2
         public static DataSource getSharedDataSource() {
             return BloclyApplication.getSharedInstance().getDataSource();
@@ -21,7 +23,7 @@ import android.app.Application;
         private static BloclyApplication sharedInstance;
         private DataSource dataSource;
 
-        // #3
+        // #3 - This method is used to initialize all of the shared application elements, in our case the DataSource.
         @Override
         public void onCreate() {
             super.onCreate();
--- blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -2,18 +2,31 @@ package io.bloc.android.blocly.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import io.bloc.android.blocly.BloclyApplication;
 import io.bloc.android.blocly.R;
 
 /**
  * Created by tonyk_000 on 3/9/2015.
  */
 public class BloclyActivity extends Activity{
+
+    public TextView mAppTitle;
+
 @Override
     protected void onCreate(Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_blocly);
+    Toast.makeText(this,
+            BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
+            Toast.LENGTH_LONG).show();
+
+    TextView mAppTitle = (TextView) findViewById(R.id.appTitle);
+    mAppTitle.setText(R.string.app_title);
 }
+
 
 
     }
